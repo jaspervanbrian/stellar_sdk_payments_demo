8edebb5586ce0c1f4cf643f34c8e5f4e2d33cf36
Jasper Martin <jaspervanbrianmartin@gmail.com>
2018-08-29 11:44:56 +0800

    Initial commit.

---
+++ lib/stellar_sdk_payments_demo.rb
@@ -0,0 +1,5 @@
+require "stellar_sdk_payments_demo/version"
+
+module StellarSdkPaymentsDemo
+  # Your code goes here...
+end
---
+++ lib/stellar_sdk_payments_demo/version.rb
@@ -0,0 +1,3 @@
+module StellarSdkPaymentsDemo
+  VERSION = "0.1.0"
+end
