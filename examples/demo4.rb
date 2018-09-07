c65b80f250e2fcc7c5471b5f51718cdbc57a5f9b
Jasper Martin <jaspervanbrianmartin@gmail.com>
2018-09-07 12:37:49 +0800

    Add another test account
    Include alphanum12 (Forgot to include it
    last time.)

--- examples/demo4.rb
+++ examples/demo4.rb
@@ -1,11 +1,12 @@
 require 'stellar_sdk_payments_demo'
 
-# Alphanum4 fiat/other assets
+# Alphanum4/Alphanum12 fiat/other assets
 
 SOURCE_SEED = "SBGWU2LWO3HHLALCREMUDNGYD2Q5R53K26ZBC4PTMGM5LF53MZRC6Q4Y"
 DESTINATION = "GDUEG27WOR4QD56KLT3WVWT4PPLPUTPX2KMKPA27DBA43ZV2DH3D5I5D"
 asset = "XRP"
 asset_type = "alphanum4"
+# asset_type = "alphanum12"
 amount = 100;
 
 StellarSdkPaymentsDemo::Client.send_payment(SOURCE_SEED, DESTINATION, amount, asset, asset_type)
